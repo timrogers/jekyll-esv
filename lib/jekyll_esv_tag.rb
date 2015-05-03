29fe976be9fe48b8a0131432683176a9de63f683
Tim Rogers <tim@gocardless.com>
2015-05-03 17:29:06 +0100

    Moving things around

--- lib/jekyll_esv_tag.rb
+++ lib/jekyll_esv_tag.rb
@@ -1,7 +1,7 @@
 require 'uri'
 
-module Jekyll
-  class EsvTag < Liquid::Tag
+module JekyllEsvTag
+  class Tag < Liquid::Tag
     URL_ROOT = "http://esvbible.org/".freeze
 
     def initialize(tag_name, text, tokens)
@@ -15,4 +15,4 @@ module Jekyll
   end
 end
 
-Liquid::Template.register_tag('esv', Jekyll::EsvTag)
+Liquid::Template.register_tag('esv', JekyllEsvTag::Tag)
